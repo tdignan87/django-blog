03690d47f6d27360f227588571b0c3ac37a8e724
Tom <tdignan87@gmail.com>
2020-05-19 13:07:44 +0100

    Added Heroku and postgres

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ.setdefault("SECRET_KEY", "'(_)6_9793=+trsll#9zcaq+zf3e8rt+-r00ub1d$6bhqxou=pc'")
+
+os.environ.setdefault("DATABASE_URL","postgres://febsrwtlocpvsf:8ad590e52c5fd2ffa5291dd33931d43aec4fcfa42474bbac2823203c730a1104@ec2-54-217-213-79.eu-west-1.compute.amazonaws.com:5432/dbko0uvu9folo0")
